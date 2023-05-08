d632dde6505dca36f6328aca45e9fd1a3dc64f03
sipri17 <syprymanao@gmail.com>
2023-05-09 06:45:44 +0700

    adding and trying react native seoul/masonry list

--- client/App.js
+++ client/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+
+import MasonryList from '@react-native-seoul/masonry-list';
+
+
 
 export default function App() {
+  const data = [
+    { uri: 'https://picsum.photos/id/1000/5626/3635' },
+    { uri: 'https://picsum.photos/id/1001/5616/3744' },
+    { uri: 'https://picsum.photos/id/1002/4312/2868' },
+    { uri: 'https://picsum.photos/id/1003/1181/1772' },
+    { uri: 'https://picsum.photos/id/1004/5616/3744' },
+    { uri: 'https://picsum.photos/id/1005/5760/3840' },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <MasonryList
+        data={data}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={{ margin: 5 }}>
+            <Image
+              source={{ uri: item.uri }}
+              style={{ height: 200, borderRadius: 10 }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
